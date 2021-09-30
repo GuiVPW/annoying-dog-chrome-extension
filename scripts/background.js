b3b361b7caf38757fbb280ac3b01ec3fb26e47e4
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-30 06:32:41 -0300

    refactor: move files to script folder

---
+++ scripts/background.js
@@ -0,0 +1,11 @@
+chrome.action.setIcon({
+	path: {
+		16: "../icons/16icon.png",
+		24: "../icons/24icon.png",
+		32: "../icons/32icon.png",
+		48: "../icons/48icon.png",
+		64: "../icons/64icon.png",
+		96: "../icons/96icon.png",
+		128: "../icons/128icon.png",
+	},
+})
