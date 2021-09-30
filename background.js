5977f911e307b3161c54306ffc9b55c3174d07d7
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-30 05:22:07 -0300

    feat: add multiple links and random image link

--- background.js
+++ background.js
@@ -1,6 +1,17 @@
-let color = "#3aa757"
+function updateImages() {
+	const imageLinks = [
+		"http://pm1.narvii.com/6968/c8631bd3fbeed0937c9393d3de866407af1e1215r1-1270-1269v2_uhq.jpg",
+		"https://static.wikia.nocookie.net/debatesjungle/images/c/c1/1z4FBfS.gif/revision/latest/scale-to-width-down/300?cb=20181204013628",
+		"https://cdn2.scratch.mit.edu/get_image/gallery/27070541_170x100.png",
+		"https://c.tenor.com/1_pkq4PEycYAAAAM/anoyying-dog.gif",
+		"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEUyukV6GFCsmmNtal62hUfvepcLo7O6dECjEnD5Yzyx4Ehm6-pWwrnopJGj3ewfffD6A&usqp=CAU",
+	]
 
-chrome.runtime.onInstalled.addListener(() => {
-	chrome.storage.sync.set({ color })
-	console.log("Default background color set to %cgreen", `color: ${color}`)
-})
+	images = document.querySelectorAll("img")
+
+	images.forEach(function (image) {
+		image.src = imageLinks[Math.floor(Math.random() * imageLinks.length)]
+	})
+}
+
+updateImages()
